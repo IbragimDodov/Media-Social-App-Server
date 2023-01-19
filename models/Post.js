4b7d5df05543030eb62d5abf4a743f343791b61d
IbragimDodov <utisey@gmail.com>
2023-01-19 16:38:56 +0300

    fetching data

--- models/Post.js
+++ models/Post.js
@@ -1,23 +1,24 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const PostSchema = new mongoose.Schema({
-  userId: {
-    type: String,
-    required: true,
+const PostSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      max: 500,
+    },
+    img: {
+      type: String,
+    },
+    likes: {
+      type: Array,
+      default: [],
+    },
   },
-  desc: {
-    type: String,
-    max: 50,
-  },
-  img: {
-    type: String,
-  },
-  likes: {
-    type: Array,
-    default: [],
-  }
-},
-{timestamps: true}
+  { timestamps: true }
 );
 
 module.exports = mongoose.model("Post", PostSchema);
